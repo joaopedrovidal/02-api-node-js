ea743c923cf8720b2990a42b3b039268706004a2
Joao Pedro <ppvidalsantos@gmail.com>
2026-02-05 23:05:48 -0300

    Knex, migrations, zod, dotenv

---
+++ src/database.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import knex from 'knex'
+import type { Knex } from 'knex'
+import { env } from './env/index.js'
+
+
+
+export const config: Knex.Config = {
+    client: 'sqlite3',
+    connection: {
+        filename: env.DATABASE_URL
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations'
+    }
+}
+
+export const SQL = knex(config)
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import fastify from 'fastify'
+import { SQL } from './database.js'
+import { randomUUID } from 'node:crypto'
+import { env } from './env/index.js'
 
 const app = fastify()
 
-app.get('/hello', () => {
-    return 'Hello Wolrd'
+app.get('/hello', async () => {
+    const transaction = await SQL('transactions')
+    .where('title', 'Teste2')
+    .select('*')
+
+    return transaction
 })
 
 app.listen({
-    port: 3333,
+    port: env.PORT,
 }).then(() => {
     console.log('HTTP Server Runing!')
 })
