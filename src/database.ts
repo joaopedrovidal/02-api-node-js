6cdff59eac76c949003057c54d05c9fcd697c2d8
João Pedro Vidal <contatojoaopedrovidal@gmail.com>
2026-02-08 18:38:55 -0300

    Fix: Corrigindo erro de deploy

--- src/database.ts
+++ src/database.ts
@@ -3,19 +3,24 @@ import knex from 'knex'
 import type { Knex } from 'knex'
 import { env } from './env/index.js'
 
-
-
 export const config: Knex.Config = {
-    client: env.DATABASE_CLIENT,
-    connection: env.DATABASE_CLIENT === 'sqlite'
-        ? {
-            filename: env.DATABASE_CLIENT
-        } : env.DATABASE_CLIENT,
-    useNullAsDefault: true,
-    migrations: {
-        extension: 'ts',
-        directory: './db/migrations'
-    }
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL, // sqlite usa arquivo
+        }
+      : {
+          connectionString: env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
 }
 
 export const SQL = knex(config)
